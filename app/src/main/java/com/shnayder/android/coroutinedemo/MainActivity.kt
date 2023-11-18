251067aa96bcc64c2a4f2d46cdc82d9fd4f34862
shnayder.1206 <shnayder.1206@gmail.com>
2023-11-18 11:53:05 +0700

    реализация SeekBar

--- app/src/main/java/com/shnayder/android/coroutinedemo/MainActivity.kt
+++ app/src/main/java/com/shnayder/android/coroutinedemo/MainActivity.kt
@@ -2,10 +2,35 @@ package com.shnayder.android.coroutinedemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import com.shnayder.android.coroutinedemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var count: Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.seekBar.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seek: SeekBar,
+                progress: Int, fromUser: Boolean
+            ) {
+                count = progress
+                binding.countText.text = "${count} coroutines"
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+            }
+        })
+
     }
 }
